54c2d844d17a07c93a32929050af0802cd767fdc
Kevin D Wolf <kevinw@software-logistics.com>
2017-05-04 05:50:49 -0400

    update references

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,7 +1,7 @@
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path
 Set-Location $scriptPath
 
-& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild /property:Configuration=Release /property:Platform=x86
-& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild /property:Configuration=Release /property:Platform=x64
-& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild /property:Configuration=Release /property:Platform=ARM
-& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild /property:Configuration=Release 
+& ${env:ProgramFiles(x86)}\MSBuild\15.0\Bin\MSBuild /property:Configuration=Release /property:Platform=x86
+& ${env:ProgramFiles(x86)}\MSBuild\15.0\Bin\MSBuild /property:Configuration=Release /property:Platform=x64
+& ${env:ProgramFiles(x86)}\MSBuild\15.0\Bin\MSBuild /property:Configuration=Release /property:Platform=ARM
+& ${env:ProgramFiles(x86)}\MSBuild\15.0\Bin\MSBuild /property:Configuration=Release 
